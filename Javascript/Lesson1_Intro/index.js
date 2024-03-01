41c015fb5c8039cabd3dc17f04ce29644ef1e0bb
Dhaval Fatnani <dhavalfatnani017@gmail.com>
2024-03-01 14:52:03 +0530

    Started with Javascript, learned Data Types

---
+++ Javascript/Lesson1_Intro/index.js
@@ -0,0 +1,46 @@
+console.log("Hello");
+
+console.log("\nVariables: ");
+// Variables
+var test;
+test = "Testing";
+
+console.log(test);
+
+let buy;
+buy = "Buying";
+
+console.log(buy);
+
+const sell = "selling";
+
+console.log(sell);
+
+// Data Types
+
+console.log("\nData Types:");
+
+/** String */
+let userName = "Dhaval";
+
+/** Number */
+let age = 10;
+let weight = 79.6;
+
+/** Boolean */
+let hasCar = true;
+let hasHouse = false;
+
+/** Undefined */
+let gender;
+
+/** null */
+let productsOwned = null;
+
+console.log(userName, ": ", typeof userName);
+console.log(age, ": ", typeof age);
+console.log(weight, ": ", typeof weight);
+console.log(hasCar, ": ", typeof hasCar);
+console.log(hasHouse, ": ", typeof hasHouse);
+console.log(gender, ": ", typeof gender);
+console.log(productsOwned, ": ", typeof productsOwned);
